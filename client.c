b1e8d1873604a15466cd27831574a6445f3ff809
nitzansaar <112202258+nitzansaar@users.noreply.github.com>
2024-11-06 19:13:19 -0800

    client connects to serverM via TCP and serverM connects to serverA via UDP. client is able to successfully log in and get authenticated by first sending the credentials to serverM who then sends it to serverA where the authentication happens. Then serverA authenticates, sends the results to serverM, finally serverM sends the results to client

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 #include <netinet/in.h>     
 #include <sys/socket.h>     
 
-#define PORT 21693 // last 3 digits of my student id are '693'
+#define PORT 21690
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -19,12 +19,11 @@ int main(int argc, char* argv[]) {
 
     int server_fd;
     struct sockaddr_in server_address;
-    int addrlen = sizeof(server_address);    
     char buffer[1024];
     char message[100];
 
-    //create UDP socket
-    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    //create TCP socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
         perror("error creating client socket");
         return -1;
@@ -34,15 +33,21 @@ int main(int argc, char* argv[]) {
     server_address.sin_port = htons(PORT); // convert port number
     server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    // Connect to the server
+    if (connect(server_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        perror("Connection failed");
+        close(server_fd);
+        return -1;
+    }
+
     //construct authentication message
     snprintf(message, sizeof(message), "%s %s", username, password);
 
-    printf("Attempting to send to port: %d\n", PORT);
+    printf("Connected to main server\n");
     printf("Message being sent: %s\n", message);
 
-    //send message to server
-    ssize_t sent_bytes = sendto(server_fd, message, strlen(message), 0, 
-                            (struct sockaddr *)&server_address, sizeof(server_address));
+    //send message to server using TCP send
+    ssize_t sent_bytes = send(server_fd, message, strlen(message), 0);
     if (sent_bytes < 0) {
         perror("error sending authentication message");
         close(server_fd);
@@ -51,26 +56,16 @@ int main(int argc, char* argv[]) {
 
     printf("sent authentication request for username %s\n", username);
 
-    int len  = recvfrom(server_fd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
+    // Receive response using TCP recv
+    int len = recv(server_fd, buffer, sizeof(buffer) - 1, 0);
     if (len < 0) {
         perror("Error receiving response");
         close(server_fd);
         return -1;
     }
     buffer[len] = '\0';
-    printf("server response %s\n", buffer);
+    printf("server response: %s\n", buffer);
+    
     close(server_fd);
-
-
     return 0;
-
-
-
-
-
-
-
-
-
-
 }
